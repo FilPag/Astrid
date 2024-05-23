8b5c78810baa670412988bd2fd627a43cd0e7339
unknown <pagliarofilip@gmail.com>
2024-05-24 00:21:46 +0200

    closing app hides

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,16 @@ import path from 'path';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
+let mainWindow: BrowserWindow
+let shouldQuit = false
+
+const trayIcon = nativeImage.createFromPath(path.join(app.getAppPath(), '.webpack/assets/icon.png'));
+
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
   app.quit();
 }
-
 
 const createWindow = (): void => {
   mainWindow = new BrowserWindow({
@@ -21,35 +26,32 @@ const createWindow = (): void => {
   });
 
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-
   mainWindow.webContents.openDevTools();
-};
 
-let mainWindow: BrowserWindow;
-let tray: Tray 
+  mainWindow.on("close", (event: { preventDefault: () => void; }) => {
+    if (!shouldQuit) {
+      mainWindow.hide()
+      event.preventDefault();
+    }
+  });
+
+
+};
 
 app.whenReady().then(() => {
   ipcMain.handle("startStream", startStream);
 
-  const trayIcon = nativeImage.createFromPath(path.join(app.getAppPath(), '.webpack/assets/icon.png'));
-  tray = new Tray(trayIcon)
-  const contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio' },
-    { label: 'Item2', type: 'radio' },
-    { label: 'Item3', type: 'radio', checked: true },
-    { label: 'Item4', type: 'radio' }
+  const tray = new Tray(trayIcon)
+  tray.on('click', () => { mainWindow.show() })
+  const trayMenu = Menu.buildFromTemplate([
+    { label: 'Quit', type: 'normal', click: () => { shouldQuit = true; app.quit() } },
   ])
-  tray.setToolTip('This is my application.')
-  tray.setContextMenu(contextMenu)
+  tray.setToolTip('Astrid')
+  tray.setContextMenu(trayMenu)
 
   createWindow();
   mainWindow.hide()
-});
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
 });
 
 app.on("activate", () => {
@@ -93,7 +95,7 @@ let streamMonitor: NodeJS.Timeout
 const startStream = async () => {
   const srcID = await getMonitorInFocus();
 
-  if(streamMonitor === undefined){
+  if (streamMonitor === undefined) {
     streamMonitor = setInterval(streamCron, 100);
     lastMonitorId = srcID;
   }
